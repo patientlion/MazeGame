90e3ff94cb448664df9e1a74368d2689b8f12283
Patient Lion <103228157+patientlion@users.noreply.github.com>
2023-09-25 22:33:39 +1300

    creating mainmenustate class

---
+++ MazeGame/MainMenuState.cpp
@@ -0,0 +1,41 @@
+﻿#include "MainMenuState.h"
+#include <iostream>
+#include <conio.h>
+#include "StateMachineExampleGame.h"
+
+using namespace std;
+
+constexpr int kQuitKey = 113;
+constexpr char kPlay = '1';
+constexpr char kQuit = '2';
+
+MainMenuState::MainMenuState(StateMachineExampleGame* pOwner)
+    : m_pOwner(pOwner)
+{}
+
+bool MainMenuState::Update(bool processInput)
+{
+    bool shouldQuit = false;
+    if (processInput)
+    {
+        int input = _getch();
+        if (input == kQuitKey || (char)input == kQuit)
+        {
+            shouldQuit = true;
+        }
+        else if ((char)input == kPlay)
+        {
+            m_pOwner->LoadScene(StateMachineExampleGame::SceneName::Gameplay);
+        }
+    }
+    return shouldQuit;
+}
+
+void MainMenuState::Draw()
+{
+    system("cls");
+    cout << endl << endl << endl;
+    cout << "Main Menu" << endl;
+    cout << kPlay << "Press 1 to play" << endl;
+    cout << kQuit << "Press 2 to quit" << endl;
+}
